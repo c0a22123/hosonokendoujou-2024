59eba264cc2b782c2510ee7ecc30cf4ed7823f03
Shusuke Chida <c0a22098ea@edu.teu.ac.jp>
2024-06-26 11:42:45 +0900

    ログイン機能

--- docker_flask_test/app/app.py
+++ docker_flask_test/app/app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from app.database import init_db,db
-from app.model import User
+# from app.database import init_db,db
+# from app.model import User
+import mysql.connector
+import pymysql
+from pymysql.cursors import DictCursor
+from app.database import connect_db, check_db
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-<<<<<<< HEAD
-# ダミーのユーザーデータベース
-users = {'admin': 'password'}
-
 @app.route('/')
 def index():
     if 'username' in session:
-        print("a")
         return render_template('index.html', username=session['username'])
     return redirect(url_for('login'))
 
@@ -22,12 +21,10 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        if username in users and users[username] == password:
+        result = connect_db(username, password)
+        if(result):
             session['username'] = username
             return redirect(url_for('index'))
-        else:
-            return 'Invalid username/password'
-
     return render_template('login.html')
 
 @app.route('/logout')
@@ -35,37 +32,14 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
-@app.route('/show')
-def show_users():
-    all_peter = User.query.filter_by(name='peter').all()
-    how_many_peter = len(all_peter)
-    return '今Peterは{}人います'.format(how_many_peter)
 
-@app.route('/add')
-def add_user():
-    peter = User(name='peter')
-    db.session.add(peter)
-    db.session.commit()
-    return 'Peterを増やしました。'
-
-@app.route('/delete')
-def delete_user():
-    peter = User.query.filter_by(name='peter').first()
-    if peter is not None:
-        db.session.delete(peter)
-        db.session.commit()
-        return 'Peterを減らしました。'
-    else:
-        return 'Peterはひとりもいません'
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-=======
 @app.route("/")
 # def index():
 #     return render_template("index.html")
 def bingo():
     return render_template("bingo.html")
->>>>>>> bd0dec5e7331eb0af0f09edcbaed1c83e441b341
--- docker_flask_test/app/database.py
+++ docker_flask_test/app/database.py
@@ -1,9 +1,57 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
+import pymysql
+from pymysql.cursors import DictCursor
 
+def connect_db(username, password, session):
+    """
+    MySQLとの接続を行う
+    成功した場合はcheck_dbを呼び出し、
+    dbに登録されているユーザとパスワードが一致するかを確認する
+    失敗した場合はエラーを返す
+    引数はブラウザに入力されたユーザ(username)、パスワード(password)である。
+    """
+    try:
+        conn = None 
 
-db = SQLAlchemy()
+        conn = mysql.connector.connect(
+        host='db',  # Docker Composeでのサービス名を指定
+        user='root',
+        password='YES',
+        database='login_sample',
+        )
+            
+        cur = conn.cursor()
+        query = "SELECT user_name AS name, password FROM User WHERE user_name = %s"
+        cur.execute(query, (username,))
+        users = cur.fetchall()
+        print(users)
+        cur.close()
+        conn.close()
+        result = check_db(users, username, password)
 
-def init_db(app):
-    db.init_app(app)
-    Migrate(app, db)
+        return result
+
+    except pymysql.MySQLError as e:
+        print(f"Error connecting to MySQL: {e}")
+        raise Exception("MySQLサーバへの接続に失敗しました")
+    
+    
+
+def check_db(users, username, password):
+    """
+    ユーザとパスワードが一致するかを判定する関数
+    一致した場合はTrueをしなかった場合はFalseを返す
+    引数はデータベースのユーザ情報とパスワード情報が入っている(user),
+    ユーザによって入力された(username),パスワード(password)
+    """                
+    #if user and user['password'] == password:
+    password_d = ""
+    for user in users:
+        if username == user[0]:
+            password_d = user[1]
+            break  
+    if(password_d == password):
+        return True
+    else:
+        return False
