f6b138c7f466247d66bc51e402c47f16b866d033
shimada masaki <c0a22080f@edu.teu.ac.jp>
2024-10-15 14:46:01 +0900

    レイアウト整理

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,20 +1,22 @@
-const popupButton = document.getElementById('popupButton');
-const popupWindow = document.getElementById('popupWindow');
-const closeButton = document.querySelector('.close');
 
-// ボタンがクリックされたときにポップアップを表示
-popupButton.addEventListener('click', function() {
-  popupWindow.style.display = 'block';
-});
+//*base.html
+document.addEventListener("DOMContentLoaded", function() {
+  const popupButton = document.getElementById('popupButton');
+  const popupWindow = document.getElementById('popupWindow');
 
-// 閉じるボタンがクリックされたときにポップアップを非表示
-closeButton.addEventListener('click', function() {
-  popupWindow.style.display = 'none';
-});
+  // メニュータブを開閉するためのイベントリスナー
+  popupButton.addEventListener('click', function() {
+      if (popupWindow.style.display === 'block') {
+          popupWindow.style.display = 'none';
+      } else {
+          popupWindow.style.display = 'block';
+      }
+  });
 
-// ポップアップ外をクリックしたときにポップアップを非表示
-window.addEventListener('click', function(event) {
-  if (event.target == popupWindow) {
-    popupWindow.style.display = 'none';
-  }
+  // メニュー外をクリックしたら閉じる
+  document.addEventListener('click', function(event) {
+      if (!popupButton.contains(event.target) && !popupWindow.contains(event.target)) {
+          popupWindow.style.display = 'none';
+      }
+  });
 });
